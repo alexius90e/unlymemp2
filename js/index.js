3ef363019750e6270b766d424e27b832debc00a5
Aliaksei Karzhou <alexius90e@gmail.com>
2024-12-29 02:55:52 +0300

    feat: added header menu

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 import './custom-select.js';
 
 const headerBurgerButton = document.querySelector('.header__burger-button');
+const headerMenu = document.querySelector('.header__menu');
 
-if (headerBurgerButton) {
+if (headerBurgerButton && headerMenu) {
   headerBurgerButton.addEventListener('click', (event) => {
     event.currentTarget.classList.toggle('active');
+    headerMenu.classList.toggle('active');
   });
 }
