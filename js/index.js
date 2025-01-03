ca7edaa47bc70b5d23e59c3a5ff136121970cafa
Aliaksei Karzhou <alexius90e@gmail.com>
2025-01-03 19:29:49 +0300

    fix: fixed style issues

--- js/index.js
+++ js/index.js
@@ -10,3 +10,46 @@ if (headerBurgerButton && headerMenu) {
     headerMenu.classList.toggle('active');
   });
 }
+
+const headerInfo = document.querySelector('.header__info');
+
+if (headerInfo) {
+  headerInfo.addEventListener('click', (event) => {
+    const isLayout = event.currentTarget === event.target;
+    const isTitle = event.target.classList.contains('header__info-title');
+    const isLink = event.target.classList.contains('header__info-menu-item-link');
+
+    if (isLayout || isLink) {
+      event.currentTarget.classList.remove('active');
+    }
+
+    if (isTitle) {
+      event.currentTarget.classList.add('active');
+    }
+  });
+}
+
+// Today, 18 Dec, 01:59:36 GMT+1
+
+const advantagesDate = document.querySelector('.advantages__date');
+
+function updateDate() {
+  const date = new Date();
+  const dateOptions = {
+    day: 'numeric',
+    month: 'short',
+  };
+  const dateString = date.toLocaleDateString('en-GB', dateOptions);
+  const timeOptions = {
+    timeStyle: 'long',
+  };
+  const timeString = date.toLocaleTimeString('en-GB', timeOptions);
+
+  if (advantagesDate) {
+    advantagesDate.innerHTML = `Today, ${dateString}, ${timeString}`;
+  }
+}
+
+updateDate();
+
+setInterval(() => updateDate(), 1000);
